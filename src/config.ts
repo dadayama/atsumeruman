91a6e930c672d722ce658d78bf8386d890287896
dadayama <dadayamism@gmail.com>
2021-06-13 22:13:43 +0900

    ユーザー一覧を取得してメッセージを投げるよう修正

--- src/config.ts
+++
@@ -1,1 +0,0 @@
-export const TARGET_CHANNEL = 'C023S89LH42'
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import * as functions from 'firebase-functions'
 import express from 'express'
 import { WebClient, LogLevel } from '@slack/web-api'
-import { TARGET_CHANNEL } from './config'
 
 const app = express()
 const config = functions.config()
@@ -9,17 +8,25 @@ const client = new WebClient(config.slack.bot_token, {
   logLevel: LogLevel.DEBUG,
 })
 
-app.get('/notify', (_, res) => {
+app.get('/notify', async (_, res) => {
   res.sendStatus(200)
 
   try {
+    const users = await client.users.list()
+    const targetUsers = users.members
+      ? users.members.filter((user) => !user.is_bot && !user.deleted)
+      : []
+
+    const mention = targetUsers.map((targetUser) => `<@${targetUser.id}>`).join(' ')
+    const text = `${mention}\nお知らせです`
+
     client.chat.postMessage({
-      channel: TARGET_CHANNEL,
-      text: '<!channel>\nお知らせです',
+      channel: config.slack.channel,
+      text,
     })
   } catch (e) {
     console.warn(e)
   }
 })
 
-export default functions.region('asia-northeast1').https.onRequest(app)
+export default functions.https.onRequest(app)
